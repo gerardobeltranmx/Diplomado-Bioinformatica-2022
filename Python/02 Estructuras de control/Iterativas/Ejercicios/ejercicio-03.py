de7ea96705419053ca24aa5a91410c7cd77461b2
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-15 18:52:57 +0000

    --

---
+++ Python/02 Estructuras de control/Iterativas/Ejercicios/ejercicio-03.py
@@ -0,0 +1,11 @@
+inicio = int(input ("Centimetros iniciales: "))
+fin = int(input ("Centimetros finales: "))
+
+print ("%12s %8s %8s %8s" % ("centimetros", "metros", "yardas", "pulgadas"))
+for cm in range(inicio, fin + 1) :
+  metros = cm / 100
+  yardas = cm / 91.44
+  pulgadas = cm / 2.54
+  print ("%12d %8.3f %8.3f %8.3f" % (cm, metros, yardas, pulgadas))
+  
+  
