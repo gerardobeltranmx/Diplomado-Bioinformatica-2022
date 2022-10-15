b42f1b2efbd046f35bfd30406cf575b412109674
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-15 00:37:35 +0000

    --

--- Python/01 Fundamentos/Ejemplos/ejemplo-03.py
+++ Python/01 Fundamentos/Ejemplos/ejemplo-03.py
@@ -3,5 +3,3 @@ radio = int(radio)
 pi = 3.141592
 area = pi * radio * radio
 print ("El area del circulo es %.3f " % area)
-
-dd
--- Python/01 Fundamentos/Ejercicios/ejercicio-01.py
+++ Python/01 Fundamentos/Ejercicios/ejercicio-01.py
@@ -1,1 +1,9 @@
-print("Hola!!!")
+gradosCelsius = int(input("Escribe la temperatura en Celsius: "))
+# gradosCelsius = int(gradosCelsius)
+gradosFahrenheit = (gradosCelsius * 1.8) + 32
+gradosKelvin = 273.15 + gradosCelsius
+gradosRankine = (gradosCelsius * 1.8) + 491.67 
+
+print (gradosFahrenheit)
+print (gradosKelvin)
+print (gradosRankine)
