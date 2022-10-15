b8fafa940062eeaf7e764d4d68372724db6fe872
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-15 15:39:02 +0000

    --

---
+++ Python/02 Estructuras de control/Condicionales/Ejercicios/ejercicio-02.py
@@ -0,0 +1,12 @@
+edad = int(input("Indicar la edad: "))
+
+if (edad >=0) and (edad <=3) :
+  print("BEBE")
+elif (edad <=14):
+  print ("NIÑO")
+elif (edad <=18) :
+  print ("JOVEN")
+elif (edad <= 65):
+  print ("ADULTO")
+else :
+  print ("ADULTO 3RA")
