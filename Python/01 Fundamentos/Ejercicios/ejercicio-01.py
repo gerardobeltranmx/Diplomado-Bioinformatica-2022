881852555b1bff4a56d68a6dde79db2f92334663
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-15 00:47:12 +0000

    --

--- Python/01 Fundamentos/Ejercicios/ejercicio-01.py
+++ Python/01 Fundamentos/Ejercicios/ejercicio-01.py
@@ -4,6 +4,6 @@ gradosFahrenheit = (gradosCelsius * 1.8) + 32
 gradosKelvin = 273.15 + gradosCelsius
 gradosRankine = (gradosCelsius * 1.8) + 491.67 
 
-print (gradosFahrenheit)
-print (gradosKelvin)
-print (gradosRankine)
+print ("%d" % (gradosFahrenheit))
+print ("%.2f" % (gradosKelvin))
+print ("%.2f" % (gradosRankine))
