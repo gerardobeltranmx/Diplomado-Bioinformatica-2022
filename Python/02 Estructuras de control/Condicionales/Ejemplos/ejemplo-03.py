28044c238c27a4172a157ba2e11c915f7a738093
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-15 02:26:57 +0000

    --

---
+++ Python/02 Estructuras de control/Condicionales/Ejemplos/ejemplo-03.py
@@ -0,0 +1,7 @@
+numero = int (input("Escribe un numero: "))
+if numero < 0: 
+  print ("El numero es negativo")
+elif numero == 0: 
+  print ("El numero es cero") 
+else: 
+  print ("El numero es positivo")
