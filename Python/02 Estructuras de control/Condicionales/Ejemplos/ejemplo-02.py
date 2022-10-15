e1998b2621bc3ea335da608b068fac6410fd6da8
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-15 02:21:40 +0000

    --

---
+++ Python/02 Estructuras de control/Condicionales/Ejemplos/ejemplo-02.py
@@ -0,0 +1,9 @@
+num = int(input("Ingrese un número: "))
+residuo = num % 2 
+if (residuo == 0):
+  print ("El numero %d es par." % num)
+  print ("Felicidades!!!")
+else:
+  print ("El numero %d es impar." % num)
+  print ("intenta de nuevo")
+
