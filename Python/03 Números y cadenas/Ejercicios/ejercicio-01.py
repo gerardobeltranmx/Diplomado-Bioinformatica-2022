44310f5ddf22553fb9f56ceca23edf873c9da267
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-21 02:09:36 +0000

    --

---
+++ Python/03 Números y cadenas/Ejercicios/ejercicio-01.py
@@ -0,0 +1,11 @@
+texto = input ("Escribe un texto: ")
+texto = texto.upper()
+a = texto.count("A")
+e = texto.count("E")
+i = texto.count("I")
+o = texto.count("O")
+u = texto.count("U")
+
+total = a + e + i + o + u
+print(total)
+
