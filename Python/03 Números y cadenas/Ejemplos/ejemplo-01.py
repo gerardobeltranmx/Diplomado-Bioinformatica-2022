d302bb71258c06e21bf6cab61b0157f7046f0c4b
Gerardo Beltrán <contacto@gerardobeltran.mx>
2022-10-21 01:30:50 +0000

    --

---
+++ Python/03 Números y cadenas/Ejemplos/ejemplo-01.py
@@ -0,0 +1,8 @@
+nombreCompleto = input("Escribe un nombre: ")
+
+x,y,z = nombreCompleto.split(" ")
+
+nombreCompleto = x.capitalize() + " " +  y.capitalize() + " "  + z.capitalize()
+
+print (nombreCompleto)
+
